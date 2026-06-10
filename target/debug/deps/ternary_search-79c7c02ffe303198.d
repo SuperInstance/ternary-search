a088845da9d7d1ee36ffc38e862151efe7d54cd5
SuperInstance <superinstance@users.noreply.github.com>
2026-06-10 00:55:27 -0800

    auto: sync updates

--- target/debug/deps/ternary_search-79c7c02ffe303198.d
+++
@@ -1,5 +0,0 @@
-/home/phoenix/repos/ternary-search/target/debug/deps/ternary_search-79c7c02ffe303198.d: src/lib.rs
-
-/home/phoenix/repos/ternary-search/target/debug/deps/ternary_search-79c7c02ffe303198: src/lib.rs
-
-src/lib.rs:
